0d0d349c02912a98a651bf80b622e34e174d5e79
Tshiring <tshiring1814@gmail.com>
2024-04-03 17:05:08 +0545

    changed to php and components added

---
+++ Views/Login.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+		<title>Document</title>
+	</head>
+	<body>
+		<div>
+			<form action="">
+				<input type="email" />
+				<input type="password" />
+				<button type="submit">Login</button>
+			</form>
+		</div>
+	</body>
+</html>
---
+++ Views/components/nav.php
@@ -0,0 +1,10 @@
+<nav>
+  <ul>
+    <li><a href="">Home</a></li>
+    <li><a href="">About</a></li>
+  </ul>
+  <div>
+    <a href="">Login</a>
+    <a href="">Register</a>
+  </div>
+</nav>
