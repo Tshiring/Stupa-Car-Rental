1c97a44192cabe841f6b7f5543c022f2a25457ea
Tshiring <tshiring1814@gmail.com>
2024-04-06 21:20:29 +0545

    base url added and header working on

--- Views/Index.php
+++ Views/Index.php
@@ -4,11 +4,14 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
+  <title>Stupa Car Rental</title>
+  <base href="http://localhost/Car/">
+
   <link rel="stylesheet" href="../public/styles/main.min.css">
 </head>
 
 <body>
+  <?php require './components/nav.php' ?>
   This is Home
 </body>
 
--- Views/components/nav.php
+++ Views/components/nav.php
@@ -1,10 +1,14 @@
-<nav>
-  <ul>
-    <li><a href="">Home</a></li>
-    <li><a href="">About</a></li>
-  </ul>
-  <div>
-    <a href="">Login</a>
-    <a href="">Register</a>
-  </div>
-</nav>
+<header>
+  <Button>
+    <nav>
+      <ul>
+        <li><a href="">Home</a></li>
+        <li><a href="">About</a></li>
+        <li><a href="">Contact</a></li>
+      </ul>
+    </nav>
+  </Button>
+  <h1>LOGO</h1>
+  <a href="views/Login.php">Login / Register</a>
+
+</header>
