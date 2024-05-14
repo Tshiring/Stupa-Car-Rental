ce2d06cc49ad246b1f26753c8ae1d39de2303720
Tshiring <tshiring1814@gmail.com>
2024-05-14 13:24:15 +0545

    register logic

--- Views/Register.php
+++ Views/Register.php
@@ -12,12 +12,45 @@
 
 <body>
   <?php require './components/nav.php' ?>
+
+  <?php
+  if (isset($_POST["submit"])) {
+    echo "ehhee";
+    $fullName = $_POST["full_name"];
+    $email = $_POST["email"];
+    $password = $_POST["password"];
+    $confirmPassword = $_POST["confirmPassword"];
+    $errors = array();
+    if (empty($fullName) or empty($email) or empty($password) or empty($confirmPassword)) {
+      array_push($errors, "All fields are required");
+    }
+    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+      array_push($errors, "Email is not valid");
+    }
+    if (strlen($password  < 6)) {
+      array_push($errors, "Password must be 6 character long");
+    }
+    if ($password !== $confirmPassword) {
+      array_push($errors, "Password must be same");
+    }
+    if (count($errors) > 0) {
+      foreach ($errors as $error) {
+        echo "<div>$error</div>";
+      }
+    } else {
+      // insert data into database
+    }
+  }
+  ?>
+
+
   <div class="auth-form">
-    <form action="">
-      <input type="text" name="" id="">
-      <input type="email" name="" id="">
-      <input type="password" name="" id="">
-      <button type="submit">Login</button>
+    <form action="views/register.php" method="POST">
+      <input type="text" name="full_name" id="">
+      <input type="email" name="email" id="">
+      <input type="password" name="password" id="">
+      <input type="password" name="confirmPassword" id="">
+      <input type="submit" class="btn btn-primary" value="Register" name="submit">
     </form>
   </div>
 </body>
