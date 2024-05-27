485112851b9bee6f9ca7214cf4a87a638bacbb4f
ChetreNelson <128913141+ChetreNelson@users.noreply.github.com>
2024-05-27 22:42:04 +0545

    car-collection page done

--- docs/index.php
+++ docs/index.php
@@ -495,6 +495,13 @@ $date = date('Y-m-d');
           </div>
         </div>
       </div>
+      <div class="see-all-btn">
+        <button>See all Cars</button>
+        <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M1 8H15M15 8L8 15M15 8L8 1" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+        </svg>
+
+      </div>
     </div>
   </section>
 
